db139c1b942a5305142d1fef345d244e37f49d57
Pentti <poinonen0@gmail.com>
2020-07-14 14:23:48 +0300

    rest api and mongoose

--- server/app.js
+++ server/app.js
@@ -11,12 +11,23 @@ import { dishRouter } from './routes/dishRouter.js';
 import { promoRouter } from './routes/promoRouter.js';
 import { leaderRouter } from './routes/leaderRouter.js';
 
+import mongoose from 'mongoose';
+
+// MongoDB Connection
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+connect.then((db) => {
+  console.log('Connection established to the MongoDB');
+}, (err) => { console.log(err) });
+
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,6 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
@@ -31,19 +43,20 @@ app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.end('Error: ' + err);
 });
 
 export { app };
---
+++ server/models/dishes.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+import loadType from 'mongoose-currency';
+const Schema = mongoose.Schema;
+const Currency = loadType.loadType(mongoose);
+
+const commentSchema = new Schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+  },
+  comment: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+const dishSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    default: ''
+  },
+  price: {
+    type: Currency,
+    required: true,
+    min: 0,
+  },
+  featured: {
+    type: Boolean,
+    default: false
+  },
+  comments: [commentSchema]
+}, {
+  timestamps: true
+});
+
+const Dishes = mongoose.model('dish', dishSchema);
+
+export default Dishes;
