308719f5b64c80242f817612498a9cf4ea77d3fb
Pentti <poinonen0@gmail.com>
2020-07-15 13:15:37 +0300

    cookies

--- server/app.js
+++ server/app.js
@@ -33,33 +33,47 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('ap-ple-pi-e'));
 
 // basic authentication
-
 const auth = (req, res, next) => {
-  console.log(req.headers);
+  console.log(req.signedCookies);
 
-  let authHeader = req.headers.authorization;
-  if (!authHeader) {
-    const err = new Error('You are not authenticated');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+  if (!req.signedCookies.user) {
+    let authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      const err = new Error('You are not authenticated');
+
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+    const auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    const username = auth[0];
+    const password = auth[1];
+
+    if (username === 'admin' && password === 'password') {
+      res.cookie('user', 'admin', { signed: true });
+      next();
+    }
+    else {
+      const err = new Error('You are not authenticated');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    };
   }
-
-  const auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-  const username = auth[0];
-  const password = auth[1];
-
-  if(username === 'admin' && password === 'password') {
-    next();
-  } else {
-    const err = new Error('You are not authenticated');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+  else {
+    if (req.signedCookies.user === 'admin') {
+      next();
+    }
+    else {
+      const err = new Error('You are not authenticated');
+      err.status = 401;
+      return next(err);
+    };
   };
 };
 
